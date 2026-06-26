266ac81380b65d39ceb955e0420df3ad42ade9b2
NineoneKing <1959850960@qq.com>
2026-06-26 22:33:30 +0800

    加入相机平滑移动，给所有代码加注释

--- script/Player.cs
+++ script/Player.cs
@@ -3,42 +3,34 @@ using System;
 
 public partial class Player : CharacterBody2D
 {
-	public const float Speed = 300.0f;
-	public const float JumpVelocity = -800.0f;
+	public const float Speed = 300.0f;//水平移动速度
+	public const float JumpVelocity = -700.0f;//跳跃给予速度
 
 	public override void _PhysicsProcess(double delta)
 	{
 		Vector2 velocity = Velocity;
-
-		// Add the gravity.
 		if (!IsOnFloor())
 		{
-			velocity += GetGravity() * (float)delta;
+			velocity += GetGravity() * (float)delta;//重力加速度
 		}
-
-		// Handle Jump.
 		if (Input.IsActionPressed("ui_accept") && IsOnFloor())
 		{
-			velocity.Y = JumpVelocity;
+			velocity.Y = JumpVelocity;//跳
 		}
 		if (Input.IsActionJustReleased("ui_accept") && !IsOnFloor()&&velocity.Y<0)
 		{
 			velocity.Y = 0;
-		}
-		
-		// Get the input direction and handle the movement/deceleration.
-		// As good practice, you should replace UI actions with custom gameplay actions.
-		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+		}//随地大小跳
+		Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");//速度方向向量
 		if (direction != Vector2.Zero)
 		{
-			velocity.X = direction.X * Speed;
+			velocity.X = direction.X * Speed;//方向向量不等于0向量，走路
 		}
 		else
 		{
-			velocity.X = Mathf.MoveToward(Velocity.X, 0, 50);
+			velocity.X = Mathf.MoveToward(Velocity.X, 0, 50);//等于0，脚滑
 		}
-	
 		Velocity = velocity;
-		MoveAndSlide();
+		MoveAndSlide();//图灵老祖保佑我
 	}
 }
--- script/smoothCamera2d.cs
+++ script/smoothCamera2d.cs
@@ -2,13 +2,26 @@ using Godot;
 using System;
 public partial class smoothCamera2d : Camera2D
 {
-	// Called when the node enters the scene tree for the first time.
+	[Export] private NodePath nodePath;//拖入玩家节点
+	[Export] private float movementspeed_x=0.14f;
+	[Export] private float movementspeed_y=0.22f;//速度系数
+	[Export] private float minspeed_x=0.15f;
+	[Export] private float minspeed_y=1f;//最小步长
+	Node2D _node;
 	public override void _Ready()
 	{
+		_node =GetNode<Node2D>(nodePath);
 	}
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
 	public override void _PhysicsProcess(double delta)
 	{
-		
+		if (_node == null) return; // 安全防御
+		Vector2 playerposition =_node.GlobalPosition;
+		Vector2 camaraposition=GlobalPosition;
+		float distans_x=camaraposition.X-playerposition.X;
+		float distans_y=camaraposition.Y-playerposition.Y;//玩家与摄像机距离差
+		float vx=Mathf.MoveToward(camaraposition.X, playerposition.X,(distans_x*distans_x*movementspeed_x*(float)delta)+minspeed_x);
+		float vy=Mathf.MoveToward(camaraposition.Y, playerposition.Y,(distans_y*distans_y*movementspeed_y*(float)delta)+minspeed_y);//移动步长=距离差^2*速度系数*delta+最小步长
+		GlobalPosition=new Vector2(vx,vy);
+		float stepX = movementspeed_x * (float)delta;//图灵老祖保佑我
 	}
 }
